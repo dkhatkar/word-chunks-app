0e2b9f3afa60950d2f58188c118d607742a87dfb
dkhatkar <daniel.khatkar@openedgepay.com>
2019-10-25 16:13:12 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import WordChunksForm from './WordChunksForm';
 
 
 class App extends React.Component {
--- src/WordChunksForm.js
+++ src/WordChunksForm.js
@@ -1,17 +1,58 @@
 import React from 'react';
+import App from "./App";
 
 class WordChunksForm extends React.Component {
 
-  render(){
+    constructor(props){
+        super(props);
+        this.updateValue = this.updateValue.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            chunks : ""
+        };
+    }
 
-    return {
-      <form>
-        <label>
-          Words:
-          <input type="textarea" name="chunks" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    };
-  }
+
+    handleSubmit(event){
+        alert("hello");
+        var chunks = this.state.chunks;
+        var payload ={
+            chunks : chunks
+        };
+        var  stringifyPayload =  JSON.stringify(payload);
+
+        event.preventDefault();
+
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: stringifyPayload
+        };
+
+        const request = new Request("http://localhost:8070/words/chunks", options);
+        console.log(request);
+        const response =  fetch(request);
+    }
+    updateValue(event){
+        this.setState({chunks: event.target.value});
+
+    }
+    render() {
+
+        return <form onSubmit={this.handleSubmit}>
+            <div>
+                <div>
+                    <label>Words:</label>
+                </div>
+                <div>
+                    <textarea name="chunks" value={this.state.chunks} onChange={this.updateValue}/>
+                </div>
+            </div>
+            <input type="submit" value="Submit"/>
+        </form>
+
+    }
 }
+
+export default WordChunksForm;
+
